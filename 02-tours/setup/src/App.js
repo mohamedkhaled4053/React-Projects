16799a16a4a2b89b1f6b8e48544e6c5daf309330
mohamedkhaled4053 <mohamedkhaled4053@gmail.com>
2022-08-04 16:24:44 +0200

    2-tours: add Refresh component

--- 02-tours/setup/src/App.js
+++ 02-tours/setup/src/App.js
@@ -1,27 +1,37 @@
 import React, { useState, useEffect } from "react";
 import Loading from "./Loading";
 import Tours from "./Tours";
+import Refresh from "./Refresh";
 import "./index.css";
 // ATTENTION!!!!!!!!!!
 // I SWITCHED TO PERMANENT DOMAIN
 const url = "https://course-api.com/react-tours-project";
 function App() {
-  let [tours, setTours] = useState(false);
+  let [tours, setTours] = useState([]);
+  let [loading, setLoading] = useState(true);
+
+  function getData(){
+    setLoading(true)
+    fetch(url)
+    .then((res) => res.json())
+    .then((res) => {
+      setTours(res);
+      setLoading(false);
+    });
+  }
 
   useEffect(() => {
-    fetch(url)
-      .then((res) => res.json())
-      .then((res) => setTours(res));
+    getData()
   }, []);
 
   function removeTour(id) {
-    let newTours = tours.filter(t=> t.id != id)
-    setTours(newTours)
+    let newTours = tours.filter((t) => t.id != id);
+    setTours(newTours);
   }
 
   return (
     <main>
-      {!tours ? <Loading /> : <Tours tours={tours} removeTour={removeTour} />}
+      {loading ? <Loading /> : (tours.length !== 0)?<Tours tours={tours} removeTour={removeTour} />: <Refresh getData={getData}/>}
     </main>
   );
 }
---
+++ 02-tours/setup/src/Refresh.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Refresh({getData}) {
+  return (
+    <div className="title">
+      <h2>no tours left</h2>
+      <button className="btn" onClick={()=> getData()}>refresh</button>
+    </div>
+  );
+}
+
+export default Refresh;
