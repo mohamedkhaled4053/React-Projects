0c3a640d94d579b65f72e9d256ab9fdf79155bc6
mohamedkhaled4053 <mohamedkhaled4053@gmail.com>
2022-08-04 19:47:19 +0200

    4:accordion: modify App.js

--- 04-accordion/setup/src/App.js
+++ 04-accordion/setup/src/App.js
@@ -1,8 +1,19 @@
 import React, { useState } from 'react';
 import data from './data';
+import Question from './Question';
 import SingleQuestion from './Question';
 function App() {
-  return <h2>accordion project setup</h2>;
+  return <main>
+    <div className='container'>
+    <h3>questions and answers about login</h3>
+    <section className='info'>
+    {data.map(question=><SingleQuestion />)}
+
+    </section>
+
+
+    </div>
+  </main>
 }
 
 export default App;
