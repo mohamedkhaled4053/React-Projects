804262ca9e2eefbea05bff00468e3bdc49793b03
mohamedkhaled4053 <mohamedkhaled4053@gmail.com>
2022-08-04 18:00:32 +0200

    3-reviews: modify App.js

--- 03-reviews/setup/src/App.js
+++ 03-reviews/setup/src/App.js
@@ -1,7 +1,18 @@
-import React from 'react';
-import Review from './Review';
+import React from "react";
+import Review from "./Review";
 function App() {
-  return <h2>reviews project setup</h2>;
+  return (
+    <main>
+      <section className="container">
+        <div class="title">
+          <h2>our reviews</h2>
+          <div class="underline"></div>
+        </div>
+
+        <Review />
+      </section>
+    </main>
+  );
 }
 
 export default App;
