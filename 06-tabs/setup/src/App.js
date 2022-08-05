7a45c2e9374c03016c2f98a368db8ec57d581487
mohamedkhaled4053 <mohamedkhaled4053@gmail.com>
2022-08-05 10:06:46 +0200

    6-tabs: add currentUser state and loading state

--- 06-tabs/setup/src/App.js
+++ 06-tabs/setup/src/App.js
@@ -5,13 +5,23 @@ import { FaAngleDoubleRight } from "react-icons/fa";
 const url = "https://course-api.com/react-tabs-project";
 function App() {
   let [users, setUsers] = useState([]);
+  let [currentUser, setCurrentUser] = useState('')
+  let [loading, setLoading] = useState(true)
 
   useEffect(() => {
     fetch(url)
       .then((res) => res.json())
-      .then((res) => setUsers(res));
+      .then((res) => {
+        setUsers(res)
+        setCurrentUser(res[0].company)
+        setLoading(false)
+      });
   }, []);
-
+  if(loading){
+    return <section className="section">
+      <h1 style={{textAlign: 'center'}}>Loading...</h1>
+    </section>
+  }
   return (
     <section className="section">
       <div className="title">
@@ -21,12 +31,14 @@ function App() {
       <div className="job-center">
         <div class="btn-container">
           {users.map((user) => (
-            <button key={user.id} class="job-btn false">
+            <button key={user.id} class={`job-btn ${(currentUser === user.company)? 'active-btn': 'false'}`}>
               {user.company}
             </button>
           ))}
         </div>
       </div>
+
+      
 
       <button type="button" className="btn">
         more info
