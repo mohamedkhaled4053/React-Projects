75d243b40b02547e21a23382478d9ee9d460a9cf
mohamedkhaled4053 <mohamedkhaled4053@gmail.com>
2022-08-05 13:57:17 +0200

    7-slider: add static content

--- 07-slider/setup/src/App.js
+++ 07-slider/setup/src/App.js
@@ -1,9 +1,44 @@
-import React, { useState, useEffect } from 'react';
-import { FiChevronRight, FiChevronLeft } from 'react-icons/fi';
-import { FaQuoteRight } from 'react-icons/fa';
-import data from './data';
+import React, { useState, useEffect } from "react";
+import { FiChevronRight, FiChevronLeft } from "react-icons/fi";
+import { FaQuoteRight } from "react-icons/fa";
+import data from "./data";
 function App() {
-  return <h2>slider project setup</h2>;
+  return (
+    <section className="section">
+      <div className="title">
+        <h2>
+          <span>/</span>reviews
+        </h2>
+      </div>
+
+      <div className="section-center">
+      {data.map((user) => (
+        <article>
+          <img
+            src={user.image}
+            alt={user.name}
+            className="person-img"
+          />
+          <h4>{user.name}</h4>
+          <p className="title">{user.title}</p>
+          <p className="text">
+              {user.quote}
+          </p>
+          <FaQuoteRight />
+        </article>
+      ))}
+
+      <button className="prev">
+        <FiChevronLeft />
+      </button>
+      <button className="next">
+        <FiChevronRight />
+      </button>
+      </div>
+
+
+    </section>
+  );
 }
 
 export default App;
