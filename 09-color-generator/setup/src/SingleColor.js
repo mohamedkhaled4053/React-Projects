2d80708c6f7ab496f623bb7ff7526cc73ea168c5
mohamedkhaled4053 <mohamedkhaled4053@gmail.com>
2022-08-06 10:34:47 +0200

    9-color-generator: modify SingleColor.js

--- 09-color-generator/setup/src/SingleColor.js
+++ 09-color-generator/setup/src/SingleColor.js
@@ -1,8 +1,16 @@
-import React, { useState, useEffect } from 'react'
-import rgbToHex from './utils'
+import React, { useState, useEffect } from "react";
+import rgbToHex from "./utils";
 
-const SingleColor = () => {
-  return <h4>single color</h4>
-}
+const SingleColor = ({ rgb, weight, type }) => {
+  return (
+    <article
+      className={`color ${type === "shade" && "color-light"}`}
+      style={{ backgroundColor: `${rgbToHex(...rgb)}` }}
+    >
+      <p className="percent-value">{weight}%</p>
+      <p className="color-value">{rgbToHex(...rgb)}</p>
+    </article>
+  );
+};
 
-export default SingleColor
+export default SingleColor;
