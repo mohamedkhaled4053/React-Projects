0444287bdfe3491c6456dd89a7469bbb03741eb0
mohamedkhaled4053 <mohamedkhaled4053@gmail.com>
2022-08-06 10:51:44 +0200

    9-color-generator: generate colors based on input

--- 09-color-generator/setup/src/App.js
+++ 09-color-generator/setup/src/App.js
@@ -5,15 +5,20 @@ import Values from "values.js";
 
 function App() {
 
-
+  let [color, setColor] = useState('')
   let [list, setList] = useState(new Values('#f15025').all(10))
+
+  function handleSubmit(e) {
+    e.preventDefault()
+    setList(new Values(color).all(10))
+  }
 
   return (
     <>
       <section className="container">
         <h3>color generator</h3>
-        <form>
-          <input type="text" placeholder="#f15025" className="error"  />
+        <form onSubmit={handleSubmit}>
+          <input type="text" placeholder='#f15025' className="error" value={color} onChange={(e)=>setColor(e.target.value)}/>
           <button type="submit" className="btn">
             submit
           </button>
