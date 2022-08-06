c672c1c4f5285b221c03bd0f94432eeaef93bf00
mohamedkhaled4053 <mohamedkhaled4053@gmail.com>
2022-08-06 12:05:15 +0200

    9-color-generator: copy the color on click

--- 09-color-generator/setup/src/SingleColor.js
+++ 09-color-generator/setup/src/SingleColor.js
@@ -1,14 +1,35 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import rgbToHex from "./utils";
 
 const SingleColor = ({ rgb, weight, type }) => {
+  let [isClicked, setIsClicked] = useState(false);
+
+  function handleClick(e) {
+    setIsClicked(true)
+    let value = rgbToHex(...rgb)
+    navigator.clipboard.writeText(value)
+  }
+
+  useEffect(()=>{
+    let timer = setTimeout(() => {
+      setIsClicked(false)
+    }, 2000);
+
+    return ()=>{
+      clearTimeout(timer)
+    }
+
+  })
+
   return (
-    <article
+    <article onClick={handleClick}
       className={`color ${type === "shade" && "color-light"}`}
       style={{ backgroundColor: `${rgbToHex(...rgb)}` }}
     >
       <p className="percent-value">{weight}%</p>
       <p className="color-value">{rgbToHex(...rgb)}</p>
+
+      {isClicked && <p className="alert">copied to clipboard</p>}
     </article>
   );
 };
