5d25da543f3bb2521e0923a430c6c5d8a7b18804
mohamedkhaled4053 <mohamedkhaled4053@gmail.com>
2022-08-11 19:31:14 +0200

    12-sidebar-modal: add static code

--- 12-sidebar-modal/setup/src/Sidebar.js
+++ 12-sidebar-modal/setup/src/Sidebar.js
@@ -1,10 +1,38 @@
-import React from 'react'
-import logo from './logo.svg'
-import { FaTimes } from 'react-icons/fa'
-import { social, links } from './data'
+import React from "react";
+import logo from "./logo.svg";
+import { FaTimes } from "react-icons/fa";
+import { social, links } from "./data";
 
 const Sidebar = () => {
-  return <h2>sidebar</h2>
-}
+  return (
+    <aside class="sidebar show-sidebar">
+      <div class="sidebar-header">
+        <img
+          src="/static/media/logo.2bb7da65.svg"
+          class="logo"
+          alt="coding addict"
+        />
+        <button class="close-btn">
+          <FaTimes />
+        </button>
+      </div>
+      <ul class="links">
+        <li>
+          <a href="/">
+            {/* {icon here} */}
+            home
+          </a>
+        </li>
+      </ul>
+      <ul class="social-icons">
+        <li>
+          <a href="https://www.twitter.com">
+            {/* {icon here} */}
+          </a>
+        </li>
+      </ul>
+    </aside>
+  );
+};
 
-export default Sidebar
+export default Sidebar;
