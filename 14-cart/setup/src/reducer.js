92059bc7b8c9edce9c07a9dda35aabb6c9184ffd
mohamedkhaled4053 <mohamedkhaled4053@gmail.com>
2022-08-12 20:59:37 +0200

    14-cart: delete item if no amount of it

--- 14-cart/setup/src/reducer.js
+++ 14-cart/setup/src/reducer.js
@@ -1,9 +1,11 @@
 export default function reducer(state, action) {
   if (action.type === "GET_DATA") {
     return { ...state, cart: action.payload, loading: false };
+
   } else if (action.type === "DELETE") {
     let newCart = state.cart.filter((item) => item.id !== action.payload);
     return { ...state, cart: newCart };
+
   } else if (action.type === "INCREASE") {
     let newCart = state.cart.map((item) => {
       if (item.id === action.payload) {
@@ -14,7 +16,17 @@ export default function reducer(state, action) {
     });
 
     return { ...state, cart: newCart };
+
   } else if (action.type === "DECREASE") {
+    // check if there is any amount of item
+    // if no amount then delete it
+    let modifiedItem = state.cart.find((item) => item.id === action.payload);
+    if (modifiedItem.amount - 1 <= 0) {
+      let newCart = state.cart.filter((item) => item.id !== action.payload);
+      return { ...state, cart: newCart };
+    }
+
+    // if the amount is more than one then decrease it
     let newCart = state.cart.map((item) => {
       if (item.id === action.payload) {
         return { ...item, amount: item.amount - 1 };
