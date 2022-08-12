c77ccacca79621507b6da86f8e8ae0b93ac39e78
mohamedkhaled4053 <mohamedkhaled4053@gmail.com>
2022-08-12 20:15:55 +0200

    14-cart: increase and decrease

--- 14-cart/setup/src/reducer.js
+++ 14-cart/setup/src/reducer.js
@@ -4,6 +4,24 @@ export default function reducer(state, action) {
   } else if (action.type === "DELETE") {
     let newCart = state.cart.filter((item) => item.id !== action.payload);
     return { ...state, cart: newCart };
-  
+  } else if (action.type === "INCREASE") {
+    let newCart = state.cart.map((item) => {
+      if (item.id === action.payload) {
+        return { ...item, amount: item.amount + 1 };
+      } else {
+        return item;
+      }
+    });
+
+    return { ...state, cart: newCart };
+  } else if (action.type === "DECREASE") {
+    let newCart = state.cart.map((item) => {
+      if (item.id === action.payload) {
+        return { ...item, amount: item.amount - 1 };
+      } else {
+        return item;
+      }
+    });
+    return { ...state, cart: newCart };
   }
 }
