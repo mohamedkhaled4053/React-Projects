6f7848c7e023a03909e6603e87af277ea620b7e3
mohamedkhaled4053 <mohamedkhaled4053@gmail.com>
2022-08-23 11:39:44 +0200

    15-cocktails: format all files

--- 15-cocktails/setup/src/index.js
+++ 15-cocktails/setup/src/index.js
@@ -1,12 +1,12 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import './index.css'
-import App from './App'
-import { AppProvider } from './context'
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import { AppProvider } from './context';
 ReactDOM.render(
   <React.StrictMode>
     <AppProvider>
-    <App />
+      <App />
     </AppProvider>
   </React.StrictMode>,
   document.getElementById('root')
