e869d49bcf43615228329f68e7c1148be1134030
mohamedkhaled4053 <mohamedkhaled4053@gmail.com>
2022-08-23 17:43:10 +0200

    15-cocktails: edit SearchForm component

--- 15-cocktails/setup/src/components/SearchForm.js
+++ 15-cocktails/setup/src/components/SearchForm.js
@@ -1,18 +1,25 @@
 import React from 'react';
+import { useEffect } from 'react';
 import { useGlobalContext } from '../context';
 
 const SearchForm = () => {
   let { query, setQuery } = useGlobalContext();
 
+  let searchInput = React.useRef(null)
+  useEffect(()=>{
+    searchInput.current.focus()
+  })
+
   return (
     <section className="section search">
-      <form className="search-form">
+      <form className="search-form" onSubmit={(e) => e.preventDefault()}>
         <div className="form-control">
           <label htmlFor="name">search your favorite cocktail</label>
           <input
             type="text"
             name="name"
             id="name"
+            ref={searchInput}
             value={query}
             onChange={(e) => setQuery(e.target.value)}
           />
