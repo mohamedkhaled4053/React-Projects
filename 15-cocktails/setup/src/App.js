6ce1230a4ed434d009ca3556a9442b1f9863c248
mohamedkhaled4053 <mohamedkhaled4053@gmail.com>
2022-08-23 11:40:25 +0200

    15-cocktails: setup routes

--- 15-cocktails/setup/src/App.js
+++ 15-cocktails/setup/src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 // import pages
 import Home from './pages/Home';
 import About from './pages/About';
@@ -9,9 +9,15 @@ import Error from './pages/Error';
 import Navbar from './components/Navbar';
 function App() {
   return (
-    <div>
-      <h2>app component</h2>
-    </div>
+    <BrowserRouter>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/cocktail/:id" element={<SingleCocktail />} />
+        <Route path="*" element={<Error />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
