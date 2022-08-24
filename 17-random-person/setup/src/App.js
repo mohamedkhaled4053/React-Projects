ea5f5218006def46535558e0c79a298dc96cfe0a
mohamedkhaled4053 <mohamedkhaled4053@gmail.com>
2022-08-24 14:05:36 +0200

    17-random-person: add static code

--- 17-random-person/setup/src/App.js
+++ 17-random-person/setup/src/App.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState, useEffect } from 'react';
 import {
   FaEnvelopeOpen,
   FaUser,
@@ -6,11 +6,49 @@ import {
   FaMap,
   FaPhone,
   FaLock,
-} from 'react-icons/fa'
-const url = 'https://randomuser.me/api/'
-const defaultImage = 'https://randomuser.me/api/portraits/men/75.jpg'
+} from 'react-icons/fa';
+const url = 'https://randomuser.me/api/';
+const defaultImage = 'https://randomuser.me/api/portraits/men/75.jpg';
 function App() {
-  return <h2>random user starter</h2>
+  return (
+    <main>
+      <div className="block bcg-black"></div>
+      <div className="block">
+        <div className="container">
+          <img
+            src="https://randomuser.me/api/portraits/men/72.jpg"
+            alt="random user"
+            className="user-img"
+          />
+          <p className="user-title">My age is</p>
+          <p className="user-value">60</p>
+          <div className="values-list">
+            <button className="icon" data-label="name">
+              <FaUser />
+            </button>
+            <button className="icon" data-label="email">
+              <FaEnvelopeOpen />
+            </button>
+            <button className="icon" data-label="age">
+              <FaCalendarTimes />
+            </button>
+            <button className="icon" data-label="street">
+              <FaMap />
+            </button>
+            <button className="icon" data-label="phone">
+              <FaPhone />
+            </button>
+            <button className="icon" data-label="password">
+              <FaLock />
+            </button>
+          </div>
+          <button className="btn" type="button">
+            random user
+          </button>
+        </div>
+      </div>
+    </main>
+  );
 }
 
-export default App
+export default App;
