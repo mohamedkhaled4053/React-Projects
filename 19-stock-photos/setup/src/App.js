a603c8048eef7a7e4358f183800c7c7820a72f89
mohamedkhaled4053 <mohamedkhaled4053@gmail.com>
2022-08-26 11:27:14 +0200

    19-stock-photo: add static code

--- 19-stock-photos/setup/src/App.js
+++ 19-stock-photos/setup/src/App.js
@@ -1,12 +1,42 @@
-import React, { useState, useEffect } from 'react'
-import { FaSearch } from 'react-icons/fa'
-import Photo from './Photo'
-// const clientID = `?client_id=${process.env.REACT_APP_ACCESS_KEY}`
-const mainUrl = `https://api.unsplash.com/photos/`
-const searchUrl = `https://api.unsplash.com/search/photos/`
+import React, { useState, useEffect } from 'react';
+import { FaSearch } from 'react-icons/fa';
+import Photo from './Photo';
+
+const clientID = `?client_id=${process.env.REACT_APP_ACCESS_KEY}`;
+const mainUrl = `https://api.unsplash.com/photos/`;
+const searchUrl = `https://api.unsplash.com/search/photos/`;
 
 function App() {
-  return <h2>stock photos starter</h2>
+  let [photos, setPhotos] = useState([])
+
+  useEffect(()=>{
+    let firstUrl = mainUrl + clientID
+    fetch(firstUrl).then(res => res.json()).then(res => setPhotos(res))
+  },[])
+
+  return (
+    <main>
+      <section className="search">
+        <form className="search-form">
+          <input
+            type="text"
+            placeholder="search"
+            className="form-input"
+            value=""
+          />
+          <button type="submit" className="submit-btn">
+            <FaSearch />
+          </button>
+        </form>
+      </section>
+      <section className="photos">
+        <div className="photos-center">
+          {photos.map(photo => <Photo key={photo.id} {...photo}/>)}
+        </div>
+        <h2 className="loading">Loading...</h2>
+      </section>
+    </main>
+  );
 }
 
-export default App
+export default App;
--- 19-stock-photos/setup/src/Photo.js
+++ 19-stock-photos/setup/src/Photo.js
@@ -1,7 +1,29 @@
-import React from 'react'
+import React from 'react';
 
-const Photo = () => {
-  return <h2>photo component</h2>
-}
+const Photo = ({
+  user: {
+    name,
+    portfolio_url,
+    profile_image: { medium },
+  },
+  urls: { regular: img },
+  likes,
+  alt_description,
+}) => {
+  return (
+    <article className="photo">
+      <img src={img} alt={alt_description} />
+      <div className="photo-info">
+        <div>
+          <h4>{name}</h4>
+          <p>{likes} likes</p>
+        </div>
+        <a href={portfolio_url}>
+          <img src={medium || null} alt="" className="user-img" />
+        </a>
+      </div>
+    </article>
+  );
+};
 
-export default Photo
+export default Photo;
