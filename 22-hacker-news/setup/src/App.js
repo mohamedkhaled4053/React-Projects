863d4163252ae92fe4f881887e2a523f0a436b4d
mohamedkhaled4053 <mohamedkhaled4053@gmail.com>
2022-08-27 19:54:14 +0200

    22-hacker-news: add static code

--- 22-hacker-news/setup/src/App.js
+++ 22-hacker-news/setup/src/App.js
@@ -1,9 +1,24 @@
-import React from 'react'
-import SearchForm from './SearchForm'
-import Stories from './Stories'
-import Buttons from './Buttons'
+import React from 'react';
+import SearchForm from './SearchForm';
+import Stories from './Stories';
+import Buttons from './Buttons';
 function App() {
-  return <h2>hacker news starter</h2>
+  return (
+    <>
+      <form class="search-form">
+        <h2>search hacker news</h2>
+        <input type="text" class="form-input" value="react" />
+      </form>
+
+      <div class="btn-container">
+        <button>prev</button>
+        <p>3 of 50</p>
+        <button>next</button>
+      </div>
+
+      <Stories />
+    </>
+  );
 }
 
-export default App
+export default App;
--- 22-hacker-news/setup/src/Stories.js
+++ 22-hacker-news/setup/src/Stories.js
@@ -1,9 +1,29 @@
-import React from 'react'
+import React from 'react';
 
-import { useGlobalContext } from './context'
+import { useGlobalContext } from './context';
 
 const Stories = () => {
-  return <h2>stories component</h2>
-}
+  return (
+    <section class="stories">
+      <article class="story">
+        <h4 class="title">On React and WordPress</h4>
+        <p class="info">
+          543 points by <span>firasd | </span> 201 comments
+        </p>
+        <div>
+          <a
+            href="https://ma.tt/2017/09/on-react-and-wordpress/"
+            class="read-link"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            read more
+          </a>
+          <button class="remove-btn">remove</button>
+        </div>
+      </article>
+    </section>
+  );
+};
 
-export default Stories
+export default Stories;
