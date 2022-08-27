e963505eb7a226138c7b080e646dc0c91aac2a96
mohamedkhaled4053 <mohamedkhaled4053@gmail.com>
2022-08-27 15:00:53 +0200

    21-movie-db: remove a warning

--- 21-movie-db/setup/src/SearchForm.js
+++ 21-movie-db/setup/src/SearchForm.js
@@ -6,7 +6,7 @@ const SearchForm = () => {
     <form className="search-form">
       <h2>search movies</h2>
       <input type="text" className="form-input" value={query} onChange={(e)=>setQuery(e.target.value)} />
-      {error && <div class="error">{error}</div>}
+      {error && <div className="error">{error}</div>}
     </form>
   );
 };
