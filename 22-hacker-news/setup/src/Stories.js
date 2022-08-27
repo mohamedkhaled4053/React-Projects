2eeec67469a6a04264631c052528c341ad10c41d
mohamedkhaled4053 <mohamedkhaled4053@gmail.com>
2022-08-27 20:40:22 +0200

    22-hacker-news: display articles

--- 22-hacker-news/setup/src/Stories.js
+++ 22-hacker-news/setup/src/Stories.js
@@ -3,25 +3,33 @@ import React from 'react';
 import { useGlobalContext } from './context';
 
 const Stories = () => {
+  let { loading, articles } = useGlobalContext();
+
+  if (loading) {
+    return <div className="loading"></div>;
+  }
+
   return (
     <section className="stories">
-      <article className="story">
-        <h4 className="title">On React and WordPress</h4>
-        <p className="info">
-          543 points by <span>firasd | </span> 201 comments
-        </p>
-        <div>
-          <a
-            href="https://ma.tt/2017/09/on-react-and-wordpress/"
-            className="read-link"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            read more
-          </a>
-          <button className="remove-btn">remove</button>
-        </div>
-      </article>
+      {articles.map(({title, url, author, points, num_comments}) => (
+        <article className="story">
+          <h4 className="title">{title}</h4>
+          <p className="info">
+            {points} points by <span>{author} | </span> {num_comments} comments
+          </p>
+          <div>
+            <a
+              href={url}
+              className="read-link"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              read more
+            </a>
+            <button className="remove-btn">remove</button>
+          </div>
+        </article>
+      ))}
     </section>
   );
 };
