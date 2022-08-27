2aada6871c37a68443656e2dbeea884b281be790
mohamedkhaled4053 <mohamedkhaled4053@gmail.com>
2022-08-27 14:46:33 +0200

    21-movie-db: add static code to single movie page

--- 21-movie-db/setup/src/SingleMoviePage.js
+++ 21-movie-db/setup/src/SingleMoviePage.js
@@ -1,9 +1,27 @@
-import React, { useState, useEffect } from 'react'
-import { useParams, Link } from 'react-router-dom'
-import { API_ENDPOINT } from './context'
+import React, { useState, useEffect } from 'react';
+import { useParams, Link } from 'react-router-dom';
+import { API_ENDPOINT } from './context';
 
 const SingleMovie = () => {
-  return <h2>single movie</h2>
-}
+  return (
+    <section className="single-movie">
+      <img
+        src="https://m.media-amazon.com/images/M/MV5BOTY4YjI2N2MtYmFlMC00ZjcyLTg3YjEtMDQyM2ZjYzQ5YWFkXkEyXkFqcGdeQXVyMTQxNzMzNDI@._V1_SX300.jpg"
+        alt="Batman Begins"
+      />
+      <div className="single-movie-info">
+        <h2>Batman Begins</h2>
+        <p>
+          After training with his mentor, Batman begins his fight to free
+          crime-ridden Gotham City from corruption.
+        </p>
+        <h4>2005</h4>
+        <Link className="btn" to="/">
+          back to movies
+        </Link>
+      </div>
+    </section>
+  );
+};
 
-export default SingleMovie
+export default SingleMovie;
