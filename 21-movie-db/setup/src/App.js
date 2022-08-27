9016345c10ad17d6a22697c01e37fa73a0970f19
mohamedkhaled4053 <mohamedkhaled4053@gmail.com>
2022-08-27 13:46:37 +0200

    21-movie-db: rename a file

--- 21-movie-db/setup/src/App.js
+++ 21-movie-db/setup/src/App.js
@@ -2,7 +2,7 @@ import React from 'react'
 import { Switch, Route } from 'react-router-dom'
 
 import Home from './Home'
-import Movie from './SingleMovie'
+import Movie from './SingleMoviePage'
 
 function App() {
   return <h2>movie DB starter</h2>
