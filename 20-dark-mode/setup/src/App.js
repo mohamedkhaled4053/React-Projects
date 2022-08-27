f345c0a86c5a128c0769a3bc7814a8ec200ffcff
mohamedkhaled4053 <mohamedkhaled4053@gmail.com>
2022-08-27 10:58:32 +0200

    20-dark-mode: add static code

--- 20-dark-mode/setup/src/App.js
+++ 20-dark-mode/setup/src/App.js
@@ -1,9 +1,21 @@
-import React, { useState, useEffect } from 'react'
-import data from './data'
-import Article from './Article'
+import React, { useState, useEffect } from 'react';
+import data from './data';
+import Article from './Article';
 
 function App() {
-  return <h2>dark mode starter</h2>
+  return (
+    <main>
+      <nav>
+        <div class="nav-center">
+          <h1>overreacted</h1>
+          <button class="btn">toggle</button>
+        </div>
+      </nav>
+      <section class="articles">
+        {data.map(article => <Article key={article.id} {...article}/>)}
+      </section>
+    </main>
+  );
 }
 
-export default App
+export default App;
--- 20-dark-mode/setup/src/Article.js
+++ 20-dark-mode/setup/src/Article.js
@@ -1,7 +1,16 @@
-import React from 'react'
-import moment from 'moment'
-const Article = () => {
-  return <h2>article component</h2>
-}
+import React from 'react';
+import moment from 'moment';
+const Article = ({ title, date, length, snippet }) => {
+  return (
+    <article class="post">
+      <h2>{title}</h2>
+      <div class="post-info">
+        <span>{date}</span>
+        <span>{length} min read</span>
+      </div>
+      <p>{snippet}</p>
+    </article>
+  );
+};
 
-export default Article
+export default Article;
