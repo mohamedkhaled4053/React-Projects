17334b6eddbdd25bc78e6053226507e2c4177429
mohamedkhaled4053 <mohamedkhaled4053@gmail.com>
2022-08-27 13:33:51 +0200

    21-movie-db: initial commit

--- 21-movie-db/setup/src/index.js
+++ 21-movie-db/setup/src/index.js
@@ -5,10 +5,10 @@ import App from './App'
 import { AppProvider } from './context'
 import { BrowserRouter as Router } from 'react-router-dom'
 ReactDOM.render(
-  <React.StrictMode>
+  <>
     <AppProvider>
       <App />
     </AppProvider>
-  </React.StrictMode>,
+  </>,
   document.getElementById('root')
 )
